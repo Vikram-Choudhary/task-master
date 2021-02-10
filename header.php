227a1500a213fb17beb9c092e4adf67c664e0eb7
Vikram-Choudhary <vikramus4@gmail.com>
2021-02-11 00:16:11 +0530

    header updated

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
+  <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg" />
   <title>Taskmaster</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
